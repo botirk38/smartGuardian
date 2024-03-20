4d9b0003e4d7f3d08610d4a17108b52abaffeb35
Botir Khaltaev <ZLAC172@live.rhul.ac.uk>
2024-03-20 09:47:24 +0000

    Fix bug in contact route handler

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,34 +1,30 @@
-import { NextResponse } from "next/server";
-import { Resend } from 'resend';
+import { Resend } from "resend";
 import { EmailContactTemplate } from "@/components/ui/email-template-contact";
 
+export async function POST(request: Request) {
+    const contactInfoResponse = await request.json();
 
+    const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST(request: NextResponse) {
-	const contactInfoResponse = await request.json();
+    const { data, error } = await resend.emails.send({
+        from: `onboarding@resend.dev`,
+        to: [
+            "btrghstk@gmail.com",
+            "yassinekj07@gmail.com",
+            "oudrhiriyouneslfim@gmail.com",
+        ],
+        subject: `Contact email from ${contactInfoResponse.contactEmail} for SmartGuardian`,
+        react: EmailContactTemplate({
+            firstName: contactInfoResponse.firstName,
+            lastName: contactInfoResponse.lastName,
+            message: contactInfoResponse.message,
+        }) as React.ReactElement,
+    });
 
-	const resend = new Resend(process.env.RESEND_API_KEY);
+    if (error) {
+        console.error(error);
+        return Response.json({ error });
+    }
 
-
-
-	const { data, error } = await resend.emails.send({
-		from: `onboarding@resend.dev`,
-		to: ['btrghstk@gmail.com','yassinekj07@gmail.com', 'oudrhiriyouneslfim@gmail.com'],
-		subject: `Contact email from ${contactInfoResponse.contactEmail} for SmartGuardian`,
-		react: EmailContactTemplate({ firstName: contactInfoResponse.firstName, lastName: contactInfoResponse.lastName, message: contactInfoResponse.message }) as React.ReactElement
-
-
-	})
-
-	if (error) {
-		console.error(error);
-		return NextResponse.json({ error })
-
-	}
-
-	return Response.json({ data });
-
-
-
-
+    return Response.json({ data });
 }
